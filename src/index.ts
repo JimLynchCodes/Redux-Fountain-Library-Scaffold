0c010cc399b024e4a908ac011386902f4c6217c4
JimTheMan <mrdotjim@gmail.com>
2016-07-01 14:31:03 -0400

    initial scaffold

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+/// <reference path="../typings/index.d.ts"/>
+
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import 'todomvc-app-css/index.css';
+import {provideStore, combineReducers} from '@ngrx/store';
+
+import {todos, visibility, initialTodo, initialVisibility} from './app/reducers/todos';
+
+import './index.scss';
+
+import {enableProdMode, provide} from '@angular/core';
+import {UIRouterConfig, UIROUTER_PROVIDERS, UiView} from 'ui-router-ng2';
+import {LocationStrategy, PathLocationStrategy, PlatformLocation} from '@angular/common';
+import {BrowserPlatformLocation} from '@angular/platform-browser';
+import {MyUIRouterConfig} from './routes';
+
+declare var process: any;
+if (process.env.NODE_ENV === 'production') {
+  enableProdMode();
+}
+
+bootstrap(UiView, [
+  ...UIROUTER_PROVIDERS,
+  provide(LocationStrategy, {useClass: PathLocationStrategy}),
+  provide(PlatformLocation, {useClass: BrowserPlatformLocation}),
+  provide(UIRouterConfig, {useClass: MyUIRouterConfig}),
+  provideStore(combineReducers({todos, visibility}), {todos: [initialTodo], visibility: initialVisibility})
+]);
